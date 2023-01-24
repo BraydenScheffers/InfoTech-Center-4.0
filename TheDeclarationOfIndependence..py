879131e13a3fe50fb6636fa02144d1af39ded606
Brayden Scheffers <02brayden.scheffers@gulllakecs.org>
2023-01-24 14:07:28 -0500

    Add ... to Loading Page

--- TheDeclarationOfIndependence..py
+++ TheDeclarationOfIndependence..py
@@ -8,15 +8,30 @@ drivers know that the InfoTech Center 4.0 OS is loading.
 """
 
 # Import Libraries Here
-from time import sleep
+import time
+import sys  # I imported the system library for further use in code.
 
 """
 import time
 time.sleep (secs) 
 """
 
-print ("\n\nWelcome - InfoTech Center 4.0 OS is Loading")
+x = 0
+a = 0
 
-sleep (2)
+print ("\n\nWelcome - InfoTech Center 4.0")
+
+time.sleep (2)
+print (" ")
+while x != 20:
+    x += 1
+    b = ("Operation Fury OS is Loading " + "." * a)
+    a = a + 1
+    sys.stdout.write('\r'+b) # \r prints a carriage return first, so `b` is printed on top of the previous line.
+    time.sleep(0.5)
+    if a == 4:
+        a = 0
+    if x == 20:
+        print(' Done')
 
 
